6cbc04d19434a7d987e9be498d21c631a01b4cf5
Gabriel <gzanella202@gmail.com>
2024-06-17 21:52:40 -0300

    Commit

--- index.php
+++ index.php
@@ -1,62 +1,94 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Academia Cleber & Cunha</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        .navbar {
+            background-color: #000;
+        }
+        .navbar-brand, .navbar-nav .nav-link {
+            color: #fff;
+        }
+        .navbar-brand:hover, .navbar-nav .nav-link:hover {
+            color: #aaa;
+        }
+        .card {
+            border: none;
+            width:30%; /* Definindo largura de 60% */
+            margin: 0 auto; /* Centralizando o cartão na tela */
+            height: 480px; /* Definindo altura fixa para os cartões */
+            overflow: hidden; /* Para garantir que as imagens não ultrapassem a altura definida */
+        }
+        .card-img {
+            height: auto;
+            max-width: 100%;
+            width: 100%;
+        }
+        .card-img-overlay {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            color: white;
+            text-shadow: 2px 2px 4px #000;
+            font-size: 1.5rem;
+            font-weight: bold;
+        }
+        .footer {
+            text-align: center;
+            margin-top: 50px;
+        }
+        .main-content {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 70vh;
+            gap: 20px;
+        }
+    </style>
 </head>
-<body class="">
-
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="">
-            <a class="navbar-brand" href="index.php">Home</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+<body>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">Academia Cleber & Cunha</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
             </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="treinos/listar_treinos.php">Treinos</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 
+    <div class="container main-content">
+        <div class="card">
+            <img src="https://i.pinimg.com/originals/2d/8b/09/2d8b090b82b1f601941f35b2c138ac18.jpg" class="card-img" alt="Login">
+            <div class="card-img-overlay">
+                <a href="login.php" class="stretched-link text-white">Login</a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="https://i.pinimg.com/originals/76/cb/8d/76cb8d189f3d08d74040e4b4f07c9395.jpg " class="card-img" alt="Register">
+            <div class="card-img-overlay">
+                <a href="register.php" class="stretched-link text-white">Cadastre-se</a>
+            </div>
+        </div>
+    </div>
 
-    <?php
-    session_start();
-    include 'config.php';
+    <div class="footer">
+    <p>Comece hoje para não se arrepender amanhã</p>
+    </div>
 
-    include 'includes/header.php';
-    
-
-    if (isset($_SESSION['user_id'])) {
-        echo "<a href='treinos/listar_treinos.php' class='btn btn-primary'>Listar Treinos</a><br>";
-        echo "<a href='logout.php' class='btn btn-primary'>Sair</a>";
-        // echo "Bora treinar, " . htmlspecialchars($_SESSION['user_name']) . "!";
-        if ($_SESSION['user_type'] == 'professor') {
-            echo "<a href='treinos/criar_treino.php' class='btn btn-primary'>Criar Treino</a><br>";
-        }
-        
-    } else {
-        //echo "Você não está logado.";
-        
-        echo "<div class='d-grid gap-2 col-4 mx-auto'>";
-        echo "<a href='login.php' class='btn btn-primary btn btn-dark btn btn-primary btn-sm'>Fazer login</a>";
-        echo "<br></br>";
-        echo "<a href='register.php' class='btn btn-dark btn btn-secondary btn-sm'>Registrar-se</a>";
-        echo "<div>";
-    }
-
-    // echo "<div>";
-    // include 'includes/footer.php';
-    // echo "<div>";
-    
-    ?> 
-
-<div class="footer">
-    <hr>
-    <footer class="">
+    <div class="footer">
         <p>&copy; 2024 Academia Cleber & Cunha</p>
-    </footer>
-    <div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+ojBxj3OB3pGZV8eqMZrW1l/ArxNf" crossorigin="anonymous"></script>
 </body>
 </html>
-
-
-
