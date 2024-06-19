34dacd31864b1ef036a6933d53a93a5cad908537
Luan Almeida <luan.henrique.almeida@hotmail.com.br>
2024-06-18 22:38:24 -0300

    Commit

--- index.php
+++ index.php
@@ -63,6 +63,16 @@
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <span class="nav-link">Bem-vindo, <?php echo htmlspecialchars($username); ?>!</span>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Sair</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 
